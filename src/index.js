dab4c2bca2fe2c9af6028cb49f90c902af743d4c
mustapha <moustaphaoussama1818@gmail.com>
2024-11-22 10:48:07 +0100

    Feat : Write the tab-switching logic inside index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,23 @@ import { aboutPageLoad } from "./aboutPage.js";
 
 
 let myContentDiv = document.querySelector('#content');
-myContentDiv.appendChild( aboutPageLoad() );
+myContentDiv.appendChild( initialPageLoad() );
+
+//add event listener to each btn in header
+const btns = document.querySelectorAll('nav button');
+const [ homeBtn, menuBtn ,aboutBtn  ] = btns;
+
+homeBtn.addEventListener('click', () => {
+    myContentDiv.innerHTML = '';
+    myContentDiv.appendChild( initialPageLoad() )
+})
+
+menuBtn.addEventListener('click', () => {
+    myContentDiv.innerHTML = '';
+    myContentDiv.appendChild( menuPageLoad() );
+})
+
+aboutBtn.addEventListener('click', () => {
+    myContentDiv.innerHTML = '';
+    myContentDiv.appendChild( aboutPageLoad() );
+})
